6b68644529c6fa709a68e651e65808a92141bafe
Vitaliy Sbain <vitaliy-sabin@yandex.ru>
2023-04-19 16:52:49 +0300

    Created DataStore

---
+++ Homework - ContactList/DataStore.swift
@@ -0,0 +1,26 @@
+//
+//  DataStore.swift
+//  Homework - ContactList
+//
+//  Created by Виталий Сабин on 19.04.2023.
+//
+
+let names = [
+    "Jon", "Oberyn", "Syrio", "Tyrion", "Ned",
+    "Teon", "Aaron", "Tim", "Ted", "Steven"
+]
+
+let surnames = [
+    "Snow", "Martell", "Forel", "Lannister", "Stark",
+    "Smith", "Dow", "Isaacson", "Pennyworth", "Jankins"
+]
+
+let phoneNumbers = [
+    "11111", "22222", "33333", "44444", "55555",
+    "66666", "77777", "88888", "99999", "12345"
+]
+
+let emails = [
+    "aaa@mail.com", "bbb@mail.com", "ccc@mail.com", "ddd@mail.com", "eee@mail.com",
+    "fff@mail.com", "ggg@mail.com", "hhh@mail.com", "iii@mail.com", "jjj@mail.com"
+]
